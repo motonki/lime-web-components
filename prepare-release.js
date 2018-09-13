8ee90f01417e189d14bd69e95c012abc043df852
Johan Groth <johan.groth@lime.tech>
2018-09-13 11:47:27 +0200

    Logging in prepare release

--- prepare-release.js
+++ prepare-release.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 
-fs.appendFileSync('.npmrc', `\n//npm.lundalogik.com/:_authToken=\${NPM_TOKEN}`, 'utf8');
+try {
+    fs.appendFileSync('.npmrc', `\n//npm.lundalogik.com/:_authToken=\${NPM_TOKEN}`, 'utf8');
+    console.log(`Added NPM_TOKEN to '.npmrc'`)
+}
+catch (error) {
+    console.error(error);
+}
