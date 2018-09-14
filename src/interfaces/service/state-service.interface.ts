0b6d7bd53e01615f1a70ae79c03179012194cc8d
Johan Groth <johan.groth@lime.tech>
2018-09-14 13:53:08 +0200

    State selector constants

--- src/interfaces/service/state-service.interface.ts
+++ src/interfaces/service/state-service.interface.ts
@@ -2,16 +2,33 @@
  * Service for letting a Lime web component subscribe to state changes
  */
 export interface StateService {
-    
+
     /**
      * Connect an object property to the state
      * The connected property will be updated when changes in the state occur
      *
-     * @param {string} selector selector describing what to observe in the state
+     * @param {StateSelector} selector state selector
      * @param {object} target object to update when state changes occur
      * @param {string} property name of the property on the target object to update
+     * @param {StateOptions} options options for the selector
      *
      * @returns {Function} unsubscribe function
      */
-    connect(selector: string, target: any, property: string): () => void;
+    connect(selector: StateSelector, target: any, property: string, options: StateOptions): () => void;
+}
+
+/**
+ * Selector describing what can be connected to in the state
+ */
+export enum StateSelector {
+    Limetypes = 'LIMETYPES',
+    Limeobjects = 'LIMEOBJECTS',
+    CurrentUser = 'CURRENT_USER'
+}
+
+/**
+ * Options for the state selector
+ */
+export interface StateOptions {
+    arguments?: any[]
 }
