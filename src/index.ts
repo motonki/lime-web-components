cf589e6e360b834d012d557f4f4f5c75936c0919
jte <john.tennlo@lime.tech>
2018-09-17 15:33:59 +0200

    Adds action related interfaces.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+export { LimeWebComponentAction } from './interfaces/lime-web-component-action.interface';
 export { LimeWebComponentContext } from './interfaces/lime-web-component-context.interface';
 export { LimeWebComponentPlatform } from './interfaces/lime-web-component-platform.interface';
 export { LimeWebComponent } from './interfaces/lime-web-component.interface';
 
+export { ActionService } from './interfaces/service/action-service.interface';
 export { HttpService } from './interfaces/service/http-service.interface';
 export { RouteService } from "./interfaces/service/route-service.interface";
 export { StateService, StateSelector, StateOptions } from './interfaces/service/state-service.interface';
