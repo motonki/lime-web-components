2bf83778ae2b6af9414041da9f51bf44bc85e5cb
Johan Groth <johan.groth@lime.tech>
2018-10-09 15:13:46 +0200

    docs(documentation): add documentation

---
+++ doczrc.js
@@ -0,0 +1,46 @@
+/**
+ * Docz config
+ */
+export default {
+    title: 'Lime Web Components',
+    description: 'Documentation for Lime Web Components',
+    typescript: true,
+    themeConfig: {
+        colors: {
+            primary: '#00b3a7',
+        },
+    },
+    menu: [
+        'Index',
+        'Installation',
+        {
+            name: 'Concepts',
+            menu: [
+                'Lime Web Component',
+                'Slots',
+                'Context',
+                'Platform services',
+                'State',
+                'Actions',
+                'Lime Elements'
+            ]
+        },
+        {
+            name: 'Tutorials',
+            menu: [
+                'Hello, world!',
+                'Hello, CRM!'
+            ]
+        },
+        {
+            name: 'API reference',
+            menu: [
+                'LimeWebComponent',
+                'LimeWebComponentPlatform',
+                'LimeWebComponentContext',
+                'LimeWebComponentAction',
+                'Connect'
+            ]
+        }
+    ]
+};
