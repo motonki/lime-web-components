d401380628be710387466f0245ce7cfa1650cc4c
Jens Gustafsson <jens.gustafsson@lime.tech>
2018-11-09 10:19:40 +0100

    style: prettier.

--- src/interfaces/lime-web-component-platform.interface.ts
+++ src/interfaces/lime-web-component-platform.interface.ts
@@ -3,23 +3,22 @@ import { TranslateService } from "./service/translate-service.interface";
 import { HttpService } from "./service/http-service.interface";
 import { RouteService } from "./service/route-service.interface";
 import { NotificationService } from "./service/notification-service.interface";
-import { ActionService } from "./service/action-service.interface"
-
+import { ActionService } from "./service/action-service.interface";
 
 // lib with functions that the platform can implement
 // plugins will use this interface to make calls to the platform
 
 export interface LimeWebComponentPlatform {
-  type: "LimeCRMWebClient" | "LimeCRMDesktopClient";
+    type: "LimeCRMWebClient" | "LimeCRMDesktopClient";
 
-  state: StateService;
+    state: StateService;
 
-  translate: TranslateService;
+    translate: TranslateService;
 
-  http: HttpService;
+    http: HttpService;
 
-  route: RouteService;
+    route: RouteService;
 
-  notifications: NotificationService;
-  action: ActionService;
+    notifications: NotificationService;
+    action: ActionService;
 }
