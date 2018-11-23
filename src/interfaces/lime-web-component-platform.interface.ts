0a1e8840e93947a7063524c403512021635a11b0
jte <john.tennlo@lime.tech>
2018-11-23 14:56:47 +0100

    Adds configs state decorator to the platform interface.

--- src/interfaces/lime-web-component-platform.interface.ts
+++ src/interfaces/lime-web-component-platform.interface.ts
@@ -16,6 +16,7 @@ export interface LimeWebComponentPlatform {
         limetypes: StateService;
         limeobjects: LimeobjectsStateService;
         application: StateService;
+        configs: StateService;
     };
 
     translate: TranslateService;
