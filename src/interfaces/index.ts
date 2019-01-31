2b8f91e2b9c2f1195bc7578d3eb2e163274d93c9
Johan Groth <johan.groth@lime.tech>
2019-01-31 12:28:51 +0100

    feat(query): add interface for query

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,5 +3,6 @@ export * from './lime-web-component-action.interface';
 export * from './lime-web-component-context.interface';
 export * from './lime-web-component-platform.interface';
 export * from './lime-web-component.interface';
+export * from './query.interface';
 
 export * from './service/index';
---
+++ src/interfaces/query.interface.ts
@@ -0,0 +1,31 @@
+export interface Query {
+    limetype: string;
+    limit?: number;
+    offset?: number;
+    orderBy?: object[];
+    filter?: Expression;
+    responseFormat: {
+        object?: object;
+        aggregates?: object;
+    };
+}
+
+export interface Expression {
+    key?: string;
+    op: Operator;
+    exp: any;
+}
+
+export enum Operator {
+    AND = 'AND',
+    OR = 'OR',
+    EQUALS = '=',
+    NOT = '!',
+    GREATER = '>',
+    LESS = '<',
+    IN = 'IN',
+    BEGINS = '=?',
+    LIKE = '?',
+    LESS_OR_EQUAL = '<=',
+    GREATER_OR_EQUAL = '>='
+}
