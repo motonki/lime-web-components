88ebb74112c64fb4812f45a9bb707d1e4f6036b4
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-03-11 14:43:31 +0100

    docs: fix compile-error

--- src/interfaces/service/route-service.interface.ts
+++ src/interfaces/service/route-service.interface.ts
@@ -16,13 +16,13 @@ export interface RouteService {
     limeObject(limetype: string, id: number);
 
     /**
-     * @param {string} limetype the limetype of the table to route to
-     * @param {Expression} filter filter expression to apply to the table
-     *
      * NOTE: At the moment, the table view might not be able to display what filters are applied correctly if the
      * expression contains anything that can not be directly mapped to a column filter. The table will display the
      * correct filtered selection of data, but there will be no indication of what is actually displayed to the user
      * if a complex filter expression with AND/OR groupings are used.
+     *
+     * @param {string} limetype the limetype of the table to route to
+     * @param {Expression} filter filter expression to apply to the table
      */
     table(limetype: string, filter?: Expression);
 }
