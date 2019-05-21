d5057ec0bf41e7645cd0dace1345417e9ed6f405
jte <john.tennlo@lime.tech>
2019-05-21 16:43:11 +0200

    feat(task service): support custom notificaion  message
    optional cancel action on notification

--- src/interfaces/service/task-service.interface.ts
+++ src/interfaces/service/task-service.interface.ts
@@ -9,6 +9,8 @@ export interface TaskService extends StateService {
      * Create a new background task
      * @param url url to the resource that will create the task
      * @param data task specific data
+     * @param cancelAction true to allow task creation to be cancelled
+     * @param message override for the default notification message
      */
-    create(url: string, data: any);
+    create(url: string, data: any, cancelAction: boolean, message?: string);
 }
