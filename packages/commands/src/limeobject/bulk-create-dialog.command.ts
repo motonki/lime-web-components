39745db9fb31e4bcfdfaf1b05acdea0920ccb6cc
Katja Bregenzer <katja.bregenzer@lime.tech>
2019-06-04 15:19:55 +0200

    docs: webclient core commands

--- packages/commands/src/limeobject/bulk-create-dialog.command.ts
+++ packages/commands/src/limeobject/bulk-create-dialog.command.ts
@@ -7,6 +7,32 @@ import {
 /**
  * Open a dialog for bulk creating limeobjects
  *
+ * ### Flow example
+ * Let's have a look at the general flow by going through the concrete example of adding several persons to a marketing activity:
+ * - Go to the table view of persons.
+ * - Filter everyone who should be included in the marketing activity.
+ * - Select 'Bulk create objects' form the action menu.
+ * - Select marketing activity as type of content.
+ * - Fill out the rest of the form and click 'create'.
+ * - A toast message appears and gives you 5 seconds to undo the action before it creates the corresponding task.
+ * - Another toast message will inform you after the task is completed.
+ * - If the task ended successful you can go to the participant table view and check the result.
+ *
+ * ### Configuration
+ * In order to activate the feature go to a table configuration in lime-admin to the limetype you want to bulk create from
+ * and add the following configuration:
+ *
+ * ```json
+ * "actions": [
+ * {
+ *      "id": "limeobject.bulk-create-dialog",
+ *      "params": {
+ *        "relations": [<LIST OF CREATABLE, RELATED FIELDS (AS STRINGS)>]
+ *      }
+ *    }
+ * ],
+ * ```
+ *
  * @id `limeobject.bulk-create-dialog`
  */
 @Command({
--- packages/commands/src/limeobject/create-dialog.command.ts
+++ packages/commands/src/limeobject/create-dialog.command.ts
@@ -3,6 +3,9 @@ import { Command, Limetype } from '@limetech/lime-web-components-interfaces';
 /**
  * Open a dialog for creating a new limeobject
  *
+ * The create dialog is implemented as a command so a plugin can easily replace the original dialog with a custom one.
+ * Check out the "Hello, Event!" tutorial for a detailed description on how to implement your own create dialog.
+ *
  * @id `limeobject.create-dialog`
  */
 @Command({
