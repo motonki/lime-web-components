e54aa06d5cf86368637ecec472018ba03922c8d6
Jens Gustafsson <jens.gustafsson@lime.tech>
2019-06-20 18:40:33 +0200

    build: lerna stuff...

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ git push --set-upstream origin master
 
 npm run build
 npx lerna version --loglevel=DEBUG --conventional-commits --create-release=github --yes
-npx lerna --loglevel=INFO publish from-git --registry=http://npm.lundalogik.com:4873/ --yes
+npx lerna --loglevel=INFO publish from-git --registry=http://npm.lundalogik.com:4873/ --yes --no-verify-access
