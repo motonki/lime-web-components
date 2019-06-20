84192b4b9a918b12b11a9279ac2cba460a65d0c5
Jens Gustafsson <jens.gustafsson@lime.tech>
2019-06-20 17:07:07 +0200

    build: update release script

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
-echo "USER: $GH_USERNAME"
 git remote rm origin
 git remote add origin "https://${GH_TOKEN}@github.com/lundalogik/lime-web-components.git"
+git push --set-upstream origin master
+
 npm run build
 npx lerna version --loglevel=DEBUG --conventional-commits --create-release=github --yes
 npx lerna --loglevel=INFO publish from-git --registry=http://npm.lundalogik.com:4873/
